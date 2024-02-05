6379b2a423cf74d1430542a6e1104086ac84f944
ssenarath <97403972+SSenarath@users.noreply.github.com>
2024-02-05 06:10:14 -0500

    try fixing image issue attempt 2

--- pages/productlist/index.vue
+++ pages/productlist/index.vue
@@ -16,47 +16,47 @@
 const productList = [
   {
     name: "Rolls",
-    image: "@/assets/pictures/rolls.jpg",
+    image: "public/pictures/rolls.jpg",
     url: "/productlist/rolls",
   },
   {
     name: "Pastries",
-    image: "@/assets/pictures/pastries.jpg",
+    image: "public/pictures/pastries.jpg",
     url: "/productlist/pastries",
   },
   {
     name: "Patties",
-    image: "@/assets/pictures/patties.jpg",
+    image: "public/pictures/patties.jpg",
     url: "/productlist/patties",
   },
   {
     name: "Cutlets",
-    image: "@/assets/pictures/cutlets.jpg",
+    image: "public/pictures/cutlets.jpg",
     url: "/productlist/cutlets",
   },
   {
     name: "Roti",
-    image: "@/assets/pictures/roti.jpg",
+    image: "public/pictures/roti.jpg",
     url: "/productlist/roti",
   },
   {
     name: "Hoppers",
-    image: "@/assets/pictures/hoppers.jpg",
+    image: "public/pictures/hoppers.jpg",
     url: "/productlist/hoppers",
   },
   {
     name: "Lunu Miris",
-    image: "@/assets/pictures/lunu-miris.jpg",
+    image: "public/pictures/lunu-miris.jpg",
     url: "/productlist/lunu-miris",
   },
   {
     name: "Seeni Sambal",
-    image: "@/assets/pictures/seeni-sambal.jpg",
+    image: "public/pictures/seeni-sambal.jpg",
     url: "/productlist/seeni-sambal",
   },
   {
     name: "Coconut Sambal",
-    image: "@/assets/pictures/coconut-sambal.jpg",
+    image: "public/pictures/coconut-sambal.jpg",
     url: "/productlist/coconut-sambal",
   },
 ];
